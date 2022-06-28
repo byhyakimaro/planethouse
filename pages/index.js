a08c451616079db092c7a256185f8aaa52d60b25
byhyakimaro <moderrevolution090217@gmail.com>
2022-06-28 20:00:14 -0300

    save

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.navbar}>
-      
-    </div>
+    <>  
+      <div className={styles.navbar}>
+        <div className={styles.logo}><img src="logo.png" width="90"></img></div>
+        <a href="#"><div className={styles.home}>Home</div></a>
+        <a href="#"><div className={styles.about}>Sobre</div></a>
+        <a href="#"><div className={styles.contact}>Localização</div></a>
+        <a href="https://wa.me/5511932123063"><div className={styles.contact}>11 932123063</div></a>
+      </div>
+    </>
   )
 }
