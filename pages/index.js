1609f325cb63d3d118931ec57b13e5655edeb7c1
byhyakimaro <moderrevolution090217@gmail.com>
2022-06-28 20:04:02 -0300

    save

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <a href="#"><div className={styles.home}>Home</div></a>
         <a href="#"><div className={styles.about}>Sobre</div></a>
         <a href="#"><div className={styles.contact}>Localização</div></a>
-        <a href="https://wa.me/5511932123063"><div className={styles.contact}>11 932123063</div></a>
+        <a href="https://wa.me/5511932123063"><div className={styles.contact}>932123063</div></a>
       </div>
     </>
   )
