92c9fa1f90453c6af9c163bf5f47ba417555f775
byhyakimaro <moderrevolution090217@gmail.com>
2022-06-29 10:11:59 -0300

    save

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
+import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>  
+      <Head>
+        <title>Home - Planet House</title>
+      </Head>
       <div className={styles.navbar}>
         <div className={styles.logo}><img src="logo.png" width="90"></img></div>
-        <a href="#"><div className={styles.home}>Home</div></a>
-        <a href="#"><div className={styles.about}>Sobre</div></a>
-        <a href="#"><div className={styles.contact}>Localização</div></a>
-        <a href="https://wa.me/5511932123063"><div className={styles.contact}>932123063</div></a>
+        <div className={styles.tools}>
+          <a href="#"><div className={styles.home}>Home</div></a>
+          <a href="#"><div className={styles.about}>Sobre</div></a>
+          <a href="#"><div className={styles.contact}>Localização</div></a>
+          <a href="https://wa.me/5511932123063"><div className={styles.contact}>11 932123063</div></a>
+        </div>
       </div>
     </>
   )
