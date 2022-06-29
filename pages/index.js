cda9f92244472e9057e010a0b9b5457b3b019149
byhyakimaro <moderrevolution090217@gmail.com>
2022-06-29 18:43:57 -0300

    save

--- pages/index.js
+++ pages/index.js
@@ -46,6 +46,13 @@ export default function Home() {
               Nossos móveis planejados possuem 5 anos de garantia para seu conforto e segurança em comprar conosco.
             </div>
           </div>
+          <div className={styles.warranty}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M7 25.85V31h34v-5.15ZM7 4h34q1.2 0 2.1.9.9.9.9 2.1v24q0 1.2-.9 2.1-.9.9-2.1.9h-9.7v10L24 40.3 16.7 44V34H7q-1.2 0-2.1-.9Q4 32.2 4 31V7q0-1.2.9-2.1Q5.8 4 7 4Zm0 16.45h34V7H7ZM7 31V7v24Z"/></svg>
+            <div className={styles.title}>Designer Planejados</div>
+            <div className={styles.description}>
+              Nossa equipe especializada vai desenvolver sem custo o projeto que você sonhou, milimetricamente do seu jeito.
+            </div>
+          </div>
         </div>
       </div>
       <div className={styles.buttonWhats}>
