d53edef0fdd39c1dc5dccdb2627f0f149711f942
byhyakimaro <moderrevolution090217@gmail.com>
2022-06-30 16:48:53 -0300

    save

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,11 @@ export default function Home() {
         </div>
       </div>
       <div className={styles.home}>
+        <div className={styles.homecontent}>
+          <div className={styles.title}>
+            Somos uma empresa especializada na confecção de móveis sob medidas
+          </div>
+        </div>
         <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="m24 30.8-12-12 2.15-2.15L24 26.5l9.85-9.85L36 18.8Z"/></svg>
       </div>
       <div className={styles.content} id="content">
